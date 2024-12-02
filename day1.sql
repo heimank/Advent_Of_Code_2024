905b0c1d5465e8b97ed411754f0be410aeb877df
heimank <hmk.heimank@gmail.com>
2024-12-02 14:39:32 +0000

    pseudocode for day1

---
+++ day1.sql
@@ -0,0 +1,8 @@
+-- Two tables with the first and second value respectively + AUTOINCREMENT
+-- ORDER BY 2nd column with the values 
+-- JOIN on the AUTOINCREMENTID 
+-- SUM(a.value - b.value)  
+
+CREATE TABLE (
+    id NOT NULL AUTOINCREMENT
+)
